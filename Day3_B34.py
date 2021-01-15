47f6d3b163d808d072ec85aa59dc586fb8fc7cf5
AlamArbazKhan <77499775+AlamArbazKhan@users.noreply.github.com>
2021-01-15 23:03:43 +0530

    Add files via upload

---
+++ Day3_B34.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+introduction to string methods: #hash is used for commenting
+
+
+# In[5]:
+
+
+name = 'vaibhave'
+print (name) #this is  a string
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('how to validate and confirm');get_ipython().run_line_magic('pinfo', 'confirm')
+
+
+# In[4]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[6]:
+
+
+type(name) # string ----> str
+
+
+# In[ ]:
+
+
+Note: Python is a case sensitive language.
+    # this is used to comment the lines...!
+
+
+# In[7]:
+
+
+name2 = "afroz"
+print(name2)
+
+
+# In[8]:
+
+
+type(name2)
+
+
+# In[9]:
+
+
+fullname = 'jeet singh'
+print(fullname)
+
+
+# In[10]:
+
+
+print(fullname.title()) #title() ----> first letter in capitols.
+
+
+# In[ ]:
+
+
+Req: i want the entire name in capitols...?
+
+
+# In[11]:
+
+
+print (fullname.upper())
+
+
+# In[ ]:
+
+
+Req: i want the entire name in small letters..?
+
+
+# In[12]:
+
+
+print (fullname.lower())
+
+
+# In[ ]:
+
+
+introduction to f strings:
+
+
+# In[ ]:
+
+
+gennal syntax of f strings:
+    f"custom note {placeholder1}{placeholder2}...{placeholdern}"
+
+
+# In[ ]:
+
+
+firstname = 'pavan'
+lastname = 'kumar'
+
+
+# In[ ]:
+
+
+Req: i want to get the full name...?
+
+
+# In[25]:
+
+
+fullname = f"{firstname} {lastname}"
+
+print(fullname)
+
+
+# In[21]:
+
+
+x = f"keep up the good work, {firstname} {lastname}"
+
+print(x)
+
+
+# In[ ]:
+
+
+Adding whitespaces to strings:
+
+
+# In[26]:
+
+
+favlanguages = "listpythoncjavacobalc#"
+print(favlanguages)
+
+
+# In[27]:
+
+
+print("favlanguages:\npython\nc\njava\nc++") #\n ---> as a keyword---> \n it will be printing in the new line
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ Day4_B34.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+adding whitespaces to string:
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('Req: i am collecation the data from the students of their fave languages');get_ipython().run_line_magic('pinfo', 'languages')
+
+
+# In[2]:
+
+
+print("favlanguages:pythonccobaltc++")
+
+
+# In[ ]:
+
+
+#enhancment of the code..!
+
+
+# In[3]:
+
+
+print("favlanguages:\npython\nc\ncobal\nc++") # \n ---> delimiter-- > newline delimiter
+
+
+# In[4]:
+
+
+print("favlanguages:\n\tpython\n\tc\n\tcobal\n\tc++")# \t ---> tab delimiter \t --->4 lines of space it will creating
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+Removing whitespaces from a string:
+
+
+# In[ ]:
+
+
+
+
+
+# In[5]:
+
+
+x= "python" # it is a valid and is correct
+
+print(x)
+
+
+# In[8]:
+
+
+y= "    python"  # have gap on the left side
+
+print(y)
+
+
+# In[9]:
+
+
+z= "python   "  # have gap on the right side....!
+
+print(z)
+
+
+# In[10]:
+
+
+y.lstrip() #leftstrip ===> t will strip off the space
+
+
+# In[11]:
+
+
+print(y)
+
+
+# In[12]:
+
+
+z.rstrip()
+
+
+# In[13]:
+
+
+a = "    python   " # strip ----> it will be eliminating the gap on both the sides. ---> searching time
+
+print(a)
+
+
+# In[14]:
+
+
+a.strip()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+note: python is a case sesntive language.
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+zen of python ---> principles / Rules.
+
+
+# In[15]:
+
+
+import this
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
