b55e38df71f0844cb6ff4e2d4bf27c00c27adbef
AlamArbazKhan <77499775+AlamArbazKhan@users.noreply.github.com>
2021-01-16 22:48:28 +0530

    Add files via upload

---
+++ Day5_B34.py
@@ -0,0 +1,259 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+introduction to list datatype:
+
+
+# In[ ]:
+
+
+Defination: A list is collection of items in a perticular order.
+    classification : A list is classified as an mutable datatype ( editable)
+        
+How to define a list----->?
+[]----> using the square brackets we can define the list.
+
+
+# In[ ]:
+
+
+
+
+
+# In[3]:
+
+
+students = ['amardeep','ayushi','shalini','mukesh','pavan','saniya'] # 0,1,2,3,4,5
+
+
+# In[4]:
+
+
+print(students)
+
+
+# In[5]:
+
+
+type(students)
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('Req: i want to get the shalini name in the output');get_ipython().run_line_magic('pinfo', 'output')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+Introduction to indexing:
+    
+    0,1,2,3,4....
+
+
+# In[7]:
+
+
+print(students[2])
+
+
+# In[8]:
+
+
+print(students[4])
+
+
+# In[9]:
+
+
+print(students[4].title())
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+1. how to add a new element to the list ----> done (append, insert)
+
+2. how to modify an element in the list ----> 
+
+3. how to delete the element in the list --->
+
+
+# In[ ]:
+
+
+
+
+
+# In[10]:
+
+
+print(students)
+
+
+# In[ ]:
+
+
+Req: i want to add ayushi in the above list...?
+
+
+# In[11]:
+
+
+students.append('ayushi')
+
+
+# In[12]:
+
+
+print(students)
+
+
+# In[ ]:
+
+
+Req: i want to add naveen in the above list...?
+
+
+# In[15]:
+
+
+students.append('naveen')
+
+
+# In[16]:
+
+
+print(students)
+
+
+# In[ ]:
+
+
+Req: adding at a custom place...?
+    jeet singh---> 4 place
+
+
+# In[18]:
+
+
+students.insert(4,'jeet singh')
+
+
+# In[19]:
+
+
+print(students)
+
+
+# In[20]:
+
+
+print(students[4])
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('2. how to modify an element in the list');get_ipython().run_line_magic('pinfo', 'list')
+
+
+# In[21]:
+
+
+print(students)
+
+
+# In[ ]:
+
+
+Req: i want to replace shalini name with harika...?
+
+
+# In[23]:
+
+
+students[2] = 'harika'
+
+
+# In[24]:
+
+
+print(students)
+
+
+# In[26]:
+
+
+print(students[2])
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
