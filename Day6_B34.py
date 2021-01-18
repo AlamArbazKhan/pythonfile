cde5ab36f8e22c7010d40c0ee4a5ce88f7c3bc20
AlamArbazKhan <77499775+AlamArbazKhan@users.noreply.github.com>
2021-01-18 22:43:22 +0530

    Add files via upload

---
+++ Day6_B34.py
@@ -0,0 +1,277 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+my_students = ['gagan','naveen','mukesh','kiran','iqra','kumar','saniya','afroz']
+
+
+# In[5]:
+
+
+print(my_students)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+req: how to delete the elements from the list...?
+
+
+# In[ ]:
+
+
+req: i want to delete kumar from the list....?
+
+
+# In[ ]:
+
+
+
+
+
+# In[7]:
+
+
+del my_students[5] # del is the menthod to be used for deleting the elements from the list...!
+
+
+# In[8]:
+
+
+print(my_students)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+Req: to do a temp delete...!
+
+
+# In[ ]:
+
+
+pop()----> by using this method it will be creating a carbon copy of the element deleted...!
+
+
+# In[ ]:
+
+
+
+
+
+# In[9]:
+
+
+print(my_students)
+
+req: i want to delet naveen from the above list..!
+# In[11]:
+
+
+x = my_students.pop(1)
+
+
+# In[12]:
+
+
+print(my_students) #naveen name is not here in the output
+
+
+# In[13]:
+
+
+print(x)
+
+
+# In[ ]:
+
+
+interview question : what is the difference between delete and pop.
+    
+what is the diff bet append and insert...    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+Organising the list:
+
+
+# In[ ]:
+
+
+
+
+
+# In[15]:
+
+
+cars = ['honda','audi','bmw','skoda','benz']
+
+
+# In[16]:
+
+
+print(cars)
+
+
+# In[ ]:
+
+
+
+
+req: i want to organise the list in an alphabetical order-----> A-Z
+# In[ ]:
+
+
+temp apprach : we will have the original back ------> sorted
+permanent approach : orginal order will be lost -----> sort    
+
+
+# In[ ]:
+
+
+
+
+
+# In[17]:
+
+
+print(sorted(cars)) #b,b ----> e,m
+
+
+# In[18]:
+
+
+print(cars) #original order is retained in the temp approach
+
+
+# In[ ]:
+
+
+
+
+
+# In[19]:
+
+
+cars.sort()
+
+
+# 
+
+# In[20]:
+
+
+print(cars)
+
+
+# In[21]:
+
+
+print(cars) # the changes are permanently applied by using the sort method..
+
+
+# In[ ]:
+
+
+interview question: what is the diff btw sort and sorted...
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+Req: to count the number of elements in the list...?
+
+
+# In[22]:
+
+
+len(cars)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
