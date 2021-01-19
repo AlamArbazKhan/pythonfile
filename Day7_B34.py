07e969158e1c60066d4eac470ec60e3ddb703f2a
AlamArbazKhan <77499775+AlamArbazKhan@users.noreply.github.com>
2021-01-19 22:40:11 +0530

    Add files via upload

---
+++ Day7_B34.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+continuation of list:
+
+
+# In[ ]:
+
+
+how to organise a list:
+    
+    sorted
+    sort
+    len
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+introduction to for loops: 
+
+
+# In[1]:
+
+
+students = ['amardeep','saniya','kumar','vishnu','rashmika','keerthi']
+
+
+# In[2]:
+
+
+print(students)
+
+
+# In[3]:
+
+
+type(students)
+
+
+# In[5]:
+
+
+print(f"keep up the good work,{students[0].title()}")
+
+
+# In[6]:
+
+
+print(f"keep up the good work,{students[1].title()}")
+
+
+# In[ ]:
+
+
+
+
+
+# In[9]:
+
+
+# general syntax of a for loop:
+
+
+# In[7]:
+
+
+for tempvar in mainvar: ----> temperary variable, main variable
+    print(tempvar)
+
+
+# In[8]:
+
+
+for x in students:
+    print(f"keep up the good work, {x}")
+
+
+# In[10]:
+
+
+for batman in students:
+    print(f"keep up the good work, {batman.title()}")
+
+
+# In[ ]:
+
+
+# enhancement of the code.
+
+
+# In[11]:
+
+
+for batman in students:
+    print(f"Keep up the good work, {batman.title()}")
+    print(f"i am looking forward to recieve your github urls, {batman.title()}")
+
+
+# In[12]:
+
+
+for batman in students:
+    print(f"Keep up the good work, {batman.title()}")
+    print(f"i am looking forward to recieve your github urls, {batman.title()}.\n") #\n....writing in the loop
+
+
+# In[13]:
+
+
+for tempvar in mainvar: # Indentation is the gap before the print(tempvar) "see below"
+    print(tempvar)
+
+
+# In[14]:
+
+
+for batman in students:
+    print(f"Keep up the good work, {batman.title()}") # in the loop
+    print(f"i am looking forward to recieve your github urls, {batman.title()}.\n") # in the loop
+    
+print('Thank you all of you for joining in the session') # writing out of the loop   
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+conditional statments:
+
+voting application:
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
