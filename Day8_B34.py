f2b1deaba340888e04065aae2a38d81e7c5dbaa7
AlamArbazKhan <77499775+AlamArbazKhan@users.noreply.github.com>
2021-01-21 22:38:23 +0530

    Add files via upload

---
+++ Day8_B34.py
@@ -0,0 +1,273 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+continuation of list:
+
+
+# In[ ]:
+
+
+Recap : for loop:
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+introduction to slicing of lists:
+
+
+# In[2]:
+
+
+students = ['naveen','alam','ravi','kumar','iqra','swapna','sandeep','alok']
+
+
+# In[3]:
+
+
+print(students)
+
+
+# In[4]:
+
+
+type(students)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+# general syntax of slicing:
+
+
+# In[ ]:
+
+
+var[startvalue:stopvalue:stepcount]
+
+
+# In[ ]:
+
+
+Note 1: Always the stop is exclusive and it will not present in the slice.
+    
+Note 2: Entire slicing concept work on the principles of indexing.
+    
+
+
+# In[5]:
+
+
+print(students)
+
+
+# In[ ]:
+
+
+# req:1 to inclide naveen and alam in a single slice.
+
+
+# In[7]:
+
+
+print(students[0:1])
+
+
+# In[ ]:
+
+
+Note 3: increment the index number by one always.
+
+
+# In[8]:
+
+
+print(students[0:2])
+
+
+# In[9]:
+
+
+print(students[2:4])
+
+
+# In[10]:
+
+
+print(students[4:6])
+
+
+# In[11]:
+
+
+print(students[6:8]) # last value is always exclusive
+
+
+# In[ ]:
+
+
+
+
+
+# In[12]:
+
+
+print(students)
+
+
+# In[13]:
+
+
+print(students[0:8:2])
+
+
+# In[ ]:
+
+
+introduction to negative indexing:
+
+
+# In[14]:
+
+
+print(students)
+
+
+# In[ ]:
+
+
+-1,-2,-3.....!
+
+
+# In[ ]:
+
+
+# req i want to print alok name in the output?
+
+
+# In[16]:
+
+
+print(students[7])
+
+
+# In[17]:
+
+
+print(students[-1])
+
+
+# In[18]:
+
+
+print(students[-2])
+
+
+# In[ ]:
+
+
+
+
+
+# In[19]:
+
+
+print(students)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+# req: can you print the above list in a reverse order....!
+
+
+# In[20]:
+
+
+students.reverse() # the changes will be applied permanently.
+
+print(students)
+
+
+# In[22]:
+
+
+cars = ['audi','benz','bmw','buggati','toyata','suzuki']
+
+
+# In[23]:
+
+
+print(cars)
+
+
+# In[ ]:
+
+
+# print the list in the reverse by using the concept of slicing...?
+
+
+# In[24]:
+
+
+print(cars[::-1]) # startvalue:stopvalue:stepcount    ::-1, ----> reverse of it
+
+
+# In[ ]:
+
+
+
+
+
+# In[25]:
+
+
+print(cars[::1]) # orginal order
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+conditional statments:
+    
+    if else conditions in python ***
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
