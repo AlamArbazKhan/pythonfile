ca4d30028b190065f78781c2fa9a240235db68bc
AlamArbazKhan <77499775+AlamArbazKhan@users.noreply.github.com>
2021-01-22 22:35:29 +0530

    Add files via upload

---
+++ Day9_B34.py
@@ -0,0 +1,259 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+introduction to tuple datatype:
+
+
+# In[ ]:
+
+
+defination: an imutable list is called as tuple.
+
+classification : it is classified as an imutable datatype
+    
+how do you define it ...?.......()    
+
+
+# In[ ]:
+
+
+
+
+
+# In[1]:
+
+
+students = ('rashmika','naveen','sandeep','arif')
+
+
+# In[2]:
+
+
+print(students)
+
+
+# In[3]:
+
+
+type(students) # the braket is different
+
+
+# In[ ]:
+
+
+# can we add an element to the tuple?
+
+
+# In[ ]:
+
+
+
+
+
+# In[5]:
+
+
+students.append('varun')
+
+
+# In[6]:
+
+
+students[1] ='shankar'
+
+
+# In[ ]:
+
+
+
+
+
+# In[8]:
+
+
+dimensions = (200,50)
+print(dimensions)
+
+
+# In[ ]:
+
+
+200----> 250
+
+
+# In[9]:
+
+
+dimensions = (250,50) #250...!
+print(dimensions)
+
+
+# In[ ]:
+
+
+
+
+
+# In[10]:
+
+
+print(students)
+
+
+# In[ ]:
+
+
+
+
+
+# In[11]:
+
+
+for x in students:
+    print(x)
+
+
+# In[12]:
+
+
+print(students[2])
+
+
+# In[13]:
+
+
+print(students[2].title())
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+introduction to dictionary datatype: ***
+
+
+# In[ ]:
+
+
+Defination: A dictionary is a combination of key value pairs.
+    
+classification : it is classified as an mutable datatype.
+    
+how to define a dictionary....?  {} # curly braces or flower brackets...!    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+Note 1: Dictionary are used widley across all the real world applications... **
+
+
+# In[ ]:
+
+
+
+
+
+# In[17]:
+
+
+alien = {'color':'green','points':5} # color ---> key : green ---> value
+
+
+# In[18]:
+
+
+print(alien)
+
+
+# In[19]:
+
+
+type(alien)
+
+
+# In[20]:
+
+
+print(alien)
+
+
+# In[ ]:
+
+
+# how to add new key value pairs
+
+
+# In[21]:
+
+
+alien['background'] = 'foresttheme'
+
+
+# In[22]:
+
+
+print(alien)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
