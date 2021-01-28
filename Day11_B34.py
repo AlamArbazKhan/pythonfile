9a786aa1c5d407b7df736f41abae61ef8b69a56b
AlamArbazKhan <77499775+AlamArbazKhan@users.noreply.github.com>
2021-01-28 22:42:33 +0530

    Add files via upload

---
+++ Day11_B34.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+introduction to conditional statments:
+
+
+# In[ ]:
+
+
+if else condition:
+
+
+# In[ ]:
+
+
+# general statment:
+if condition:
+    print("something")
+else:
+    print("someotherthing")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+voting application:
+    
+    age > = 18 =====> we are eligible to vote.
+
+
+# In[ ]:
+
+
+
+
+
+# In[1]:
+
+
+age = 17
+
+
+# In[4]:
+
+
+if age >= 18:
+    print("you are eligible to vote")
+else:
+    print("you are not eligible to vote")
+
+
+# In[ ]:
+
+
+
+
+
+# In[5]:
+
+
+age = 25
+
+
+# In[6]:
+
+
+if age >= 18:
+    print("you are eligible to vote")
+else:
+    print("you are not eligible to vote")
+
+
+# In[ ]:
+
+
+
+
+
+# In[7]:
+
+
+if age >= 18:
+    print("you are eligible to vote")
+    print("have you registered for the voter?")
+else:
+    print("you are not eligible to vote")
+    print("try next year!")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+Multiple conditions validation:
+
+
+# In[ ]:
+
+
+# Scenario based question:
+    > Admission for anyone under age 4 is free.
+    > Admission for anyone between the ages of 4 and 18 is $25.
+    > Admission for anyone age 18 or older is $40.
+
+
+# In[10]:
+
+
+# then solution code below ====> elif ( else if )
+
+if age < 4:
+    print("your entry is free")
+elif age < 18:
+    print("your entry ticket is 25")
+else:
+    print("you entry ticket is 40")
+
+
+# In[9]:
+
+
+age = 3
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
